b9c5a2a629264c31e82b1e9edf748288dc27ae66
Blaiseniyo <blaiseniyonkuru12@gmail.com>
2022-04-12 15:52:53 +0200

    adding the transferFrom token Logics to the contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -9,20 +9,48 @@ contract Token{
     uint256 public decimals = 18;
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function transfer(address _to , uint256 _value) public returns (bool success) {
-        require(_to != address(0));
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(msg.sender,_to, _value);
-        return true;
-    }
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor()public{
         totalSupply = 1000000 *(10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
     }
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    event Approval(address indexed owner , address indexed spender, uint256 value);
+
+    function transfer(address _to , uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);  
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from , address _to, uint256 _value) internal returns (bool success){
+        require(_to != address(0)); 
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from,_to, _value);
+        return true;
+    }
+
+    function approve(address spender, uint256 _value) public returns (bool success){
+        require(spender != address(0));
+
+        allowance[msg.sender][spender] = _value;
+        
+        emit Approval(msg.sender, spender, _value);
+
+        return true;
+    }
+
+
+    function transferFrom(address from, address to , uint256 value) public returns (bool succes){
+        allowance[from][msg.sender] = allowance[from][msg.sender].sub(value);
+        _transfer(from, to, value);
+        return true;
+    }
+
+
+
 }
