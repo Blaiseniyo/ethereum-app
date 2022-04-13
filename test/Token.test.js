7365949072ae9ae1cd828128a870dbad78a36bb4
Blaise Niyonkuru <62561244+Blaiseniyo@users.noreply.github.com>
2022-04-13 10:49:50 +0200

    ERC20 Token
    Created a fully tested ERC20 Token

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,10 +1,10 @@
-import {tokens} from "./helper";
+import {tokens , VMError} from "./helper";
 
 const Token = artifacts.require("./Token");
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract("Token", ([deployer,receiver]) => {
+contract("Token", ([deployer,receiver,spender]) => {
 
     let token;
     const name = "DApp Token";
@@ -49,25 +49,128 @@ contract("Token", ([deployer,receiver]) => {
          })
     })
 
-    describe("Send the token", ()=>{
+    describe("success", () => {
+        describe("Transfer tokens", ()=>{
+            let result;
+            let amount = tokens(100);
+            beforeEach(async()=>{
+                result = await token.transfer(receiver, amount, {from: deployer})
+            })
+    
+            it("it Should be transfer token", async()=>{
+                let balanceOf;
+                balanceOf = await token.balanceOf(deployer);
+                balanceOf.toString().should.equal(tokens(999900).toString());
+    
+                balanceOf = await token.balanceOf(receiver);
+                balanceOf.toString().should.equal(tokens(100).toString());
+                
+            })
+    
+            it("it emits a Transfer event", async()=>{
+    
+                result.logs[0].event.should.equal("Transfer");
+                result.logs[0].args.from.should.equal(deployer);
+                result.logs[0].args.to.should.equal(receiver);
+                result.logs[0].args.value.toString().should.equal(amount.toString());
+            })
+        })
+    })
 
-        it("it Should be transfer token", async()=>{
-            let balanceOf;
-            balanceOf = await token.balanceOf(deployer);
-            console.log("balance of deployer before transfer: ", balanceOf.toString());
-            balanceOf = await token.balanceOf(receiver);
-            console.log("balance of receiver before transfer: ", balanceOf.toString());
-            //transfer token
-            await token.transfer(receiver, tokens(100), {from: deployer});
-            balanceOf = await token.balanceOf(deployer);
-            console.log("balance of deployer after transfer: ", balanceOf.toString());
-            balanceOf = await token.balanceOf(receiver);
-            console.log("balance of receiver after transfer: ", balanceOf.toString());
-            // const result = await token.transfer(receiver, tokens(10));
+    describe("failure", () => {
 
-            // result.logs[0].args._from.should.equal(deployer);
-            // result.logs[0].args._to.should.equal(receiver);
-            // result.logs[0].args._value.toString().should.equal(tokens(10).toString());
+
+        it("rejects insufficient funds", async()=>{
+            let invalidAmount;
+
+            invalidAmount = tokens(100000000000);
+            await token.transfer(receiver, invalidAmount, {from: deployer}).should.be.rejected;
+
+            invalidAmount = tokens(100);
+            await token.transfer(deployer, invalidAmount, {from: receiver}).should.be.rejected;
+        })
+
+        it("rejects an invalid address", async()=>{
+            let amount = tokens(100);
+            await token.transfer(0x0, amount, {from: deployer}).should.be.rejected;
+        })
+    })
+
+
+    describe("Delageted Token Transfer", ()=>{
+
+
+        describe("success", async()=>{
+            
+            let result;
+            let amount;
+            let allowance;
+    
+            beforeEach(async()=>{
+                amount = tokens(100);
+                result =  await token.approve(spender, amount, {from: deployer})
+            })
+    
+            it("emit the Approval Event", async()=>{
+                result.logs[0].event.should.equal("Approval");
+                result.logs[0].args.owner.should.equal(deployer);
+                result.logs[0].args.spender.should.equal(spender);
+                result.logs[0].args.value.toString().should.equal(amount.toString());
+            })
+    
+            it("it should allow token transfer by the spender", async()=>{
+                
+                allowance = await token.allowance(deployer, spender);
+                allowance.toString().should.equal(amount.toString());
+            })
+
+            describe("Transfer Token to the receiver from the owner ", ()=>{
+                beforeEach(async()=>{
+                    result = await token.transferFrom(deployer, receiver, amount, {from: spender});
+                })
+    
+        
+                it("it should transfer the token from the owner to the receiver", async()=>{
+                    
+                    allowance = await token.allowance(deployer, spender);
+                    allowance.toString().should.equal("0");
+        
+                })
+        
+                it("it should update the balanceOf the receiver after delegated transfer", async()=>{
+
+                    let balanceOf = await token.balanceOf(receiver);
+                    balanceOf.toString().should.equal(amount.toString());
+            
+                })
+
+                it("it should emit a Transfer event", async()=>{
+                    
+                    result.logs[0].event.should.equal("Transfer");
+                    result.logs[0].args.from.should.equal(deployer);
+                    result.logs[0].args.to.should.equal(receiver);
+                    result.logs[0].args.value.toString().should.equal(amount.toString());
+                })
+            })
+        })
+
+        describe("failure", async()=>{
+            let amount;
+            let result;
+            beforeEach(async()=>{
+                amount = tokens(10000000000);
+                await token.approve(spender, amount, {from: deployer})
+            })
+
+            it("It should not send token unvailable tokens ", async()=>{
+                await token.transferFrom(deployer, receiver, amount, {from: spender}).should.be.rejectedWith(VMError);
+            })
+
+            it("It should not send tokens to an unknown receiver", async()=>{
+                await token.transferFrom(deployer, 0x0, amount, {from: spender}).should.be.rejected;
+            })
+
+           
         })
     })
 })
